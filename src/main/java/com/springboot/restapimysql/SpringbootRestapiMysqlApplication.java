2cfc911b895eb551cf402981bc84358fe76843cb
Dheepika <rdheepikaamrita@gmail.com>
2023-05-05 23:47:56 -0600

    inital commit

---
+++ src/main/java/com/springboot/restapimysql/SpringbootRestapiMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restapimysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiMysqlApplication.class, args);
+	}
+
+}
